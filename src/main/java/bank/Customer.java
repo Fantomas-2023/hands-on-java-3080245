c1ab3cc91c1eddb1a742e455d70bf22f437c8f48
just hidden face <128545041+Fantomas-2023@users.noreply.github.com>
2023-06-24 21:05:10 +0000

    Add Authenticator, modify Customer

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -6,6 +6,7 @@ public class Customer {
   private String userName;
   private String password;
   private int accountId;
+  private boolean authenticated;
 
   public Customer (int id, String name, String userName, String password, int accountId){
     setId(id);
@@ -13,6 +14,7 @@ public class Customer {
     setUserName(userName);
     setPassword(password);
     setAccountId(accountId);
+    setAuthenticated(false);
   }
 
 
@@ -55,5 +57,13 @@ public class Customer {
   public void setAccountId(int accountId) {
     this.accountId = accountId;
   }
-  
+
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  public void setAuthenticated(boolean authenticated) {
+    this.authenticated = authenticated;
+  }
+
 }
