220ac04119a1464012de8b237317f0ff55a22c9f
just hidden face <128545041+Fantomas-2023@users.noreply.github.com>
2023-07-24 14:14:15 +0000

    corrected pom

--- src/main/java/bank/Menu.java
+++ src/main/java/bank/Menu.java
@@ -72,6 +72,7 @@ public class Menu {
           break;
         case 4:
           Authenticator.logout(customer);
+          System.out.println("Thanks for banking at Globe Bank International!");
           break;
         default:
           System.out.println("Invalid option. Please, try again");
